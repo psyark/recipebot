27093fc1a3988d277f5e7cdf342c68c79cb3802d
psyark <psyark@gmail.com>
2022-10-06 16:57:02 +0900

    サイトごとのテスト書き直しの追加

--- sites/cookpad/cookpad_test.go
+++ sites/cookpad/cookpad_test.go
@@ -2,324 +2,34 @@ package cookpad
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"testing"
+
+	"github.com/psyark/recipebot/sites/common"
 )
 
-func ExampleParser_a() {
+func TestNewParser(t *testing.T) {
 	ctx := context.Background()
-	rcp, err := NewParser().Parse(ctx, "https://cookpad.com/recipe/1885344")
-	if err != nil {
-		panic(err)
+	tests := map[string]string{
+		"https://cookpad.com/recipe/1885344": `{"Title":"マーマレードですっきり甘☆豚のスペアリブ","Image":"https://img.cpcdn.com/recipes/1885344/m/d7fdaff65e9e0694d16801432cc6ea89?u=529143\u0026p=1545602123","IngredientGroups":[{"Name":"","Children":[{"Name":"豚のスペアリブ","Amount":"400~500g","Comment":"骨付き肉"},{"Name":"★オレンジマーマレード","Amount":"80g","Comment":""},{"Name":"★しょうゆ","Amount":"40~50cc","Comment":"こいくち"},{"Name":"★砂糖","Amount":"大さじ1~","Comment":""},{"Name":"※水","Amount":"150~200cc","Comment":""},{"Name":"☆サラダ油","Amount":"適宜","Comment":""}]}],"Steps":[{"Text":"厚手の鍋にサラダ油を熱し、豚の骨付き肉の表面に焼き目をつけます。","Images":null},{"Text":"※の水を加えて、煮立ったらアクをとります。★のマーマレードとしょうゆを加えて中火で煮ます。","Images":null},{"Text":"煮汁が半分くらいになったら、味をみて、砂糖を加えてください。","Images":null},{"Text":"煮汁がほとんどなくなるまで、煮からめながら、照りがでるように仕上げます。","Images":null}]}`,
+		"https://cookpad.com/recipe/1948575": `{"Title":"ビーフシチュー・イタリアン","Image":"https://img.cpcdn.com/recipes/1948575/m/a76fc44f26fe07abfdf08902f966df60?u=1252112\u0026p=1347192368","IngredientGroups":[{"Name":"","Children":[{"Name":"牛肉（バラ）有ればスネ肉","Amount":"1キロ","Comment":""},{"Name":"玉ねぎ","Amount":"1個","Comment":"みじん切り"},{"Name":"人参","Amount":"2本","Comment":"みじん切り"},{"Name":"セロリ","Amount":"1本","Comment":"みじん切り"},{"Name":"赤ワイン","Amount":"500cc","Comment":""},{"Name":"トマト缶","Amount":"1個","Comment":""},{"Name":"塩コショウ","Amount":"適量","Comment":""},{"Name":"小麦粉","Amount":"適量","Comment":"薄力粉"},{"Name":"粉チーズ","Amount":"お好みで","Comment":""}]}],"Steps":[{"Text":"まずはソフリットを作ります。フライパンにみじん切りにした玉ねぎ、人参、セロリをオリーブオイル（大３）で炒めます。","Images":null},{"Text":"水分が無くなるまで炒めます。本格的なソフリットは形が無くなるまで炒めますが、煮込んだら一緒なのでこの程度でもＯＫです。","Images":null},{"Text":"多めに作ってタッパーで冷凍すれば他の料理でも使えます。今回は半分を使いました。","Images":null},{"Text":"牛肉を大きめにカットして塩コショウし小麦粉を振ります。","Images":null},{"Text":"オリーブオイルで全面に香ばしく焼き色を付けます。","Images":null},{"Text":"別の鍋に肉を移し肉汁が残ったフライパンに赤ワインを入れアルコールを飛ばします。","Images":null},{"Text":"肉にソフリットと赤ワインを入れ強火で鍋を揺すりながら煮詰めていきます。","Images":null},{"Text":"これくらいまで煮詰めます。","Images":null},{"Text":"水をヒタヒタになるまで入れ（約１０００cc）トマト缶を入れて強火で熱し沸騰したら弱火で蓋をせずにコトコト煮ます。","Images":null},{"Text":"2時間以上煮たらこれくらいまで水分が無くなります。塩コショウで味を整えて好きな柔らかさになったら出来上がり。","Images":null},{"Text":"器に移してお好みで粉チーズを振りかけて下さい。","Images":null}]}`,
+		"https://cookpad.com/recipe/2032296": `{"Title":"トロトロ！牛すじカレー","Image":"https://img.cpcdn.com/recipes/2032296/m/27f0f55f9acb7eb68be9ecdadc8efd2a?u=1252112\u0026p=1353214123","IngredientGroups":[{"Name":"","Children":[{"Name":"牛すじ肉","Amount":"500g","Comment":""},{"Name":"玉ねぎ","Amount":"1個","Comment":"大"},{"Name":"ソフリット","Amount":"適量","Comment":"ビーフシチュー・イタリアンのレシピを参照して下さい"},{"Name":"赤ワイン","Amount":"200cc","Comment":""},{"Name":"水","Amount":"1500cc","Comment":""},{"Name":"バター","Amount":"大1","Comment":""},{"Name":"昆布だし","Amount":"1スティック","Comment":"顆粒"},{"Name":"トマト缶","Amount":"1缶","Comment":""},{"Name":"ローリエの葉","Amount":"3枚","Comment":""},{"Name":"カレールー","Amount":"6キューブ","Comment":""},{"Name":"カレー粉","Amount":"大2","Comment":""},{"Name":"ウスターソース","Amount":"大1","Comment":""}]}],"Steps":[{"Text":"写真の材料を用意して（ソフリットが無ければ玉ねぎ大2個をスライスにし、人参１本をさいの目切りにして下さい）","Images":null},{"Text":"オリーブオイル（サラダ油でも可）大２をひいて牛すじを入れ軽く塩コショウして焦げ目が付くまでしっかり焼いて下さい。","Images":null},{"Text":"赤ワインを入れてアルコール分を飛ばします。","Images":null},{"Text":"鍋に移してソフリット（無ければ人参）を入れ赤ワインを煮詰めます。","Images":null},{"Text":"煮詰めてる間に、ワインを拭いたフライパンにバター大１を入れ玉ねぎを炒めます。","Images":null},{"Text":"玉ねぎも鍋に入れて良く混ぜて更に煮詰めて乳化させます。","Images":null},{"Text":"水を入れて沸騰したら灰汁を綺麗に取って下さい。","Images":null},{"Text":"昆布ダシとトマト缶とローリエの葉を入れて蓋をせずに、2時間弱火でコトコト煮込みます。","Images":null},{"Text":"一旦火を止めカレールーとカレー粉、ウスターソースを入れて更に１０分煮込みます。","Images":null},{"Text":"味見をして好みの辛さに調整して完成！","Images":null},{"Text":"白ご飯に乗せて牛スジカレーの完成","Images":null},{"Text":"白ご飯の上に半熟卵を乗せて（簡単トロトロ！オムライス参照して下さい）オムカレーの完成！","Images":null}]}`,
 	}
 
-	data, _ := json.MarshalIndent(rcp, "", "  ")
-	fmt.Println(string(data))
-	// output:
-	// {
-	//   "Title": "マーマレードですっきり甘☆豚のスペアリブ",
-	//   "Image": "https://img.cpcdn.com/recipes/1885344/m/d7fdaff65e9e0694d16801432cc6ea89?u=529143\u0026p=1545602123",
-	//   "IngredientGroups": [
-	//     {
-	//       "Name": "",
-	//       "Children": [
-	//         {
-	//           "Name": "豚のスペアリブ",
-	//           "Amount": "400~500g",
-	//           "Comment": "骨付き肉"
-	//         },
-	//         {
-	//           "Name": "★オレンジマーマレード",
-	//           "Amount": "80g",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "★しょうゆ",
-	//           "Amount": "40~50cc",
-	//           "Comment": "こいくち"
-	//         },
-	//         {
-	//           "Name": "★砂糖",
-	//           "Amount": "大さじ1~",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "※水",
-	//           "Amount": "150~200cc",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "☆サラダ油",
-	//           "Amount": "適宜",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     }
-	//   ],
-	//   "Steps": [
-	//     {
-	//       "Text": "厚手の鍋にサラダ油を熱し、豚の骨付き肉の表面に焼き目をつけます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "※の水を加えて、煮立ったらアクをとります。★のマーマレードとしょうゆを加えて中火で煮ます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "煮汁が半分くらいになったら、味をみて、砂糖を加えてください。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "煮汁がほとんどなくなるまで、煮からめながら、照りがでるように仕上げます。",
-	//       "Images": null
-	//     }
-	//   ]
-	// }
-}
+	for url, want := range tests {
+		url := url
+		want := want
 
-func ExampleParser_b() {
-	ctx := context.Background()
-	rcp, err := NewParser().Parse(ctx, "https://cookpad.com/recipe/1948575")
-	if err != nil {
-		panic(err)
+		t.Run(url, func(t *testing.T) {
+			t.Parallel()
+
+			rcp, err := NewParser().Parse(ctx, url)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := common.RecipeMustBe(*rcp, want); err != nil {
+				t.Error(err)
+			}
+		})
 	}
-
-	data, _ := json.MarshalIndent(rcp, "", "  ")
-	fmt.Println(string(data))
-	// output:
-	// {
-	//   "Title": "ビーフシチュー・イタリアン",
-	//   "Image": "https://img.cpcdn.com/recipes/1948575/m/a76fc44f26fe07abfdf08902f966df60?u=1252112\u0026p=1347192368",
-	//   "IngredientGroups": [
-	//     {
-	//       "Name": "",
-	//       "Children": [
-	//         {
-	//           "Name": "牛肉（バラ）有ればスネ肉",
-	//           "Amount": "1キロ",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "玉ねぎ",
-	//           "Amount": "1個",
-	//           "Comment": "みじん切り"
-	//         },
-	//         {
-	//           "Name": "人参",
-	//           "Amount": "2本",
-	//           "Comment": "みじん切り"
-	//         },
-	//         {
-	//           "Name": "セロリ",
-	//           "Amount": "1本",
-	//           "Comment": "みじん切り"
-	//         },
-	//         {
-	//           "Name": "赤ワイン",
-	//           "Amount": "500cc",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "トマト缶",
-	//           "Amount": "1個",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "塩コショウ",
-	//           "Amount": "適量",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "小麦粉",
-	//           "Amount": "適量",
-	//           "Comment": "薄力粉"
-	//         },
-	//         {
-	//           "Name": "粉チーズ",
-	//           "Amount": "お好みで",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     }
-	//   ],
-	//   "Steps": [
-	//     {
-	//       "Text": "まずはソフリットを作ります。フライパンにみじん切りにした玉ねぎ、人参、セロリをオリーブオイル（大３）で炒めます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "水分が無くなるまで炒めます。本格的なソフリットは形が無くなるまで炒めますが、煮込んだら一緒なのでこの程度でもＯＫです。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "多めに作ってタッパーで冷凍すれば他の料理でも使えます。今回は半分を使いました。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "牛肉を大きめにカットして塩コショウし小麦粉を振ります。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "オリーブオイルで全面に香ばしく焼き色を付けます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "別の鍋に肉を移し肉汁が残ったフライパンに赤ワインを入れアルコールを飛ばします。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "肉にソフリットと赤ワインを入れ強火で鍋を揺すりながら煮詰めていきます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "これくらいまで煮詰めます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "水をヒタヒタになるまで入れ（約１０００cc）トマト缶を入れて強火で熱し沸騰したら弱火で蓋をせずにコトコト煮ます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "2時間以上煮たらこれくらいまで水分が無くなります。塩コショウで味を整えて好きな柔らかさになったら出来上がり。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "器に移してお好みで粉チーズを振りかけて下さい。",
-	//       "Images": null
-	//     }
-	//   ]
-	// }
-}
-
-func ExampleParser_c() {
-	ctx := context.Background()
-	rcp, err := NewParser().Parse(ctx, "https://cookpad.com/recipe/2032296")
-	if err != nil {
-		panic(err)
-	}
-
-	data, _ := json.MarshalIndent(rcp, "", "  ")
-	fmt.Println(string(data))
-	// output:
-	// {
-	//   "Title": "トロトロ！牛すじカレー",
-	//   "Image": "https://img.cpcdn.com/recipes/2032296/m/27f0f55f9acb7eb68be9ecdadc8efd2a?u=1252112\u0026p=1353214123",
-	//   "IngredientGroups": [
-	//     {
-	//       "Name": "",
-	//       "Children": [
-	//         {
-	//           "Name": "牛すじ肉",
-	//           "Amount": "500g",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "玉ねぎ",
-	//           "Amount": "1個",
-	//           "Comment": "大"
-	//         },
-	//         {
-	//           "Name": "ソフリット",
-	//           "Amount": "適量",
-	//           "Comment": "ビーフシチュー・イタリアンのレシピを参照して下さい"
-	//         },
-	//         {
-	//           "Name": "赤ワイン",
-	//           "Amount": "200cc",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "水",
-	//           "Amount": "1500cc",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "バター",
-	//           "Amount": "大1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "昆布だし",
-	//           "Amount": "1スティック",
-	//           "Comment": "顆粒"
-	//         },
-	//         {
-	//           "Name": "トマト缶",
-	//           "Amount": "1缶",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "ローリエの葉",
-	//           "Amount": "3枚",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "カレールー",
-	//           "Amount": "6キューブ",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "カレー粉",
-	//           "Amount": "大2",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "ウスターソース",
-	//           "Amount": "大1",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     }
-	//   ],
-	//   "Steps": [
-	//     {
-	//       "Text": "写真の材料を用意して（ソフリットが無ければ玉ねぎ大2個をスライスにし、人参１本をさいの目切りにして下さい）",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "オリーブオイル（サラダ油でも可）大２をひいて牛すじを入れ軽く塩コショウして焦げ目が付くまでしっかり焼いて下さい。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "赤ワインを入れてアルコール分を飛ばします。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "鍋に移してソフリット（無ければ人参）を入れ赤ワインを煮詰めます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "煮詰めてる間に、ワインを拭いたフライパンにバター大１を入れ玉ねぎを炒めます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "玉ねぎも鍋に入れて良く混ぜて更に煮詰めて乳化させます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "水を入れて沸騰したら灰汁を綺麗に取って下さい。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "昆布ダシとトマト缶とローリエの葉を入れて蓋をせずに、2時間弱火でコトコト煮込みます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "一旦火を止めカレールーとカレー粉、ウスターソースを入れて更に１０分煮込みます。",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "味見をして好みの辛さに調整して完成！",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "白ご飯に乗せて牛スジカレーの完成",
-	//       "Images": null
-	//     },
-	//     {
-	//       "Text": "白ご飯の上に半熟卵を乗せて（簡単トロトロ！オムライス参照して下さい）オムカレーの完成！",
-	//       "Images": null
-	//     }
-	//   ]
-	// }
 }
--- sites/delishkitchen/delishkitchen_test.go
+++ sites/delishkitchen/delishkitchen_test.go
@@ -2,205 +2,33 @@ package delishkitchen
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"testing"
+
+	"github.com/psyark/recipebot/sites/common"
 )
 
-func ExampleParser() {
+func TestNewParser(t *testing.T) {
 	ctx := context.Background()
-	rcp, err := NewParser().Parse(ctx, "https://delishkitchen.tv/recipes/148173434692567529")
-	if err != nil {
-		panic(err)
+	tests := map[string]string{
+		"https://delishkitchen.tv/recipes/148173434692567529": `{"Title":"鶏むね肉と夏野菜の酢豚風","Image":"https://image.delishkitchen.tv/recipe/148173434692567529/1.jpg?version=1624338242\u0026w=460","IngredientGroups":[{"Name":"","Children":[{"Name":"鶏むね肉","Amount":"1枚(300g)","Comment":""},{"Name":"ピーマン","Amount":"2個","Comment":""},{"Name":"なす","Amount":"1本","Comment":""},{"Name":"玉ねぎ","Amount":"1/4個","Comment":""},{"Name":"酒","Amount":"大さじ1","Comment":""},{"Name":"片栗粉","Amount":"大さじ1","Comment":""},{"Name":"ごま油","Amount":"大さじ3","Comment":""}]},{"Name":"☆たれ","Children":[{"Name":"酒","Amount":"大さじ1","Comment":""},{"Name":"砂糖","Amount":"小さじ1","Comment":""},{"Name":"酢","Amount":"大さじ2","Comment":""},{"Name":"しょうゆ","Amount":"大さじ1","Comment":""},{"Name":"ケチャップ","Amount":"大さじ2","Comment":""},{"Name":"鶏ガラスープの素","Amount":"小さじ1","Comment":""},{"Name":"片栗粉","Amount":"小さじ1","Comment":""}]}],"Steps":[{"Text":"鶏むね肉は食べやすい大きさに切る。ボウルに入れて酒、片栗粉を加えてもみこむ。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/1.jpg?version=1624338235"]},{"Text":"ピーマンは半分に切って種とわたを取り除き、食べやすい大きさに切る。なすはヘタをとり、食べやすい大きさに切る。玉ねぎは放射状に3等分に切る。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/2.jpg?version=1624338235"]},{"Text":"別のボウルに☆を入れて混ぜる(たれ)。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/3.jpg?version=1624338235"]},{"Text":"フライパンにごま油を入れて熱し、なす、玉ねぎを入れてしんなりするまで中火で炒め、ピーマンを加えて油がなじむ程度に炒めて取り出す。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/4.jpg?version=1624338235"]},{"Text":"同じフライパンに鶏むね肉を入れ、焼き色がつくまで中火で焼き、上下を返し、弱火で肉に火が通るまで焼く。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/5.jpg?version=1624338235"]},{"Text":"野菜を戻し入れ、たれを加えてとろみがつくまで炒める。","Images":["https://media.delishkitchen.tv/recipe/148173434692567529/steps/6.jpg?version=1624338235"]}]}`,
+		"https://delishkitchen.tv/recipes/236903854006862303": `{"Title":"牛すじ肉の下処理","Image":"https://image.delishkitchen.tv/recipe/236903854006862303/1.jpg?version=1624426802\u0026w=460","IngredientGroups":[{"Name":"","Children":[{"Name":"牛すじ肉","Amount":"300g","Comment":""},{"Name":"長ねぎ[青い部分]","Amount":"1本分","Comment":""},{"Name":"しょうが(薄切り)","Amount":"3枚","Comment":""},{"Name":"酒","Amount":"50cc","Comment":""}]}],"Steps":[{"Text":"鍋に牛すじ肉、牛すじ肉が浸かるくらいの水(分量外:適量)を入れてわかし、弱火で10分程煮る。","Images":["https://media.delishkitchen.tv/recipe/236903854006862303/steps/1.jpg?version=1647222138"]},{"Text":"湯を切り、アクと一緒に余分な脂とアクを流水で洗い流し、食べやすい大きさに切る。","Images":["https://media.delishkitchen.tv/recipe/236903854006862303/steps/2.jpg?version=1647222138"]},{"Text":"鍋も一度洗い、牛すじ肉を戻し入れ、牛すじ肉が浸かるくらいの水(分量外:適量)、ねぎ、しょうが、酒を入れてわかし、アクを取り除きながら1時間程弱火で煮る。","Images":["https://media.delishkitchen.tv/recipe/236903854006862303/steps/3.jpg?version=1647222138"]}]}`,
 	}
 
-	data, _ := json.MarshalIndent(rcp, "", "  ")
-	fmt.Println(string(data))
-	// output:
-	// {
-	//   "Title": "鶏むね肉と夏野菜の酢豚風",
-	//   "Image": "https://image.delishkitchen.tv/recipe/148173434692567529/1.jpg?version=1624338242\u0026w=460",
-	//   "IngredientGroups": [
-	//     {
-	//       "Name": "",
-	//       "Children": [
-	//         {
-	//           "Name": "鶏むね肉",
-	//           "Amount": "1枚(300g)",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "ピーマン",
-	//           "Amount": "2個",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "なす",
-	//           "Amount": "1本",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "玉ねぎ",
-	//           "Amount": "1/4個",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "酒",
-	//           "Amount": "大さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "片栗粉",
-	//           "Amount": "大さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "ごま油",
-	//           "Amount": "大さじ3",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     },
-	//     {
-	//       "Name": "☆たれ",
-	//       "Children": [
-	//         {
-	//           "Name": "酒",
-	//           "Amount": "大さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "砂糖",
-	//           "Amount": "小さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "酢",
-	//           "Amount": "大さじ2",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "しょうゆ",
-	//           "Amount": "大さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "ケチャップ",
-	//           "Amount": "大さじ2",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "鶏ガラスープの素",
-	//           "Amount": "小さじ1",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "片栗粉",
-	//           "Amount": "小さじ1",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     }
-	//   ],
-	//   "Steps": [
-	//     {
-	//       "Text": "鶏むね肉は食べやすい大きさに切る。ボウルに入れて酒、片栗粉を加えてもみこむ。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/1.jpg?version=1624338235"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "ピーマンは半分に切って種とわたを取り除き、食べやすい大きさに切る。なすはヘタをとり、食べやすい大きさに切る。玉ねぎは放射状に3等分に切る。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/2.jpg?version=1624338235"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "別のボウルに☆を入れて混ぜる(たれ)。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/3.jpg?version=1624338235"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "フライパンにごま油を入れて熱し、なす、玉ねぎを入れてしんなりするまで中火で炒め、ピーマンを加えて油がなじむ程度に炒めて取り出す。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/4.jpg?version=1624338235"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "同じフライパンに鶏むね肉を入れ、焼き色がつくまで中火で焼き、上下を返し、弱火で肉に火が通るまで焼く。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/5.jpg?version=1624338235"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "野菜を戻し入れ、たれを加えてとろみがつくまで炒める。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/148173434692567529/steps/6.jpg?version=1624338235"
-	//       ]
-	//     }
-	//   ]
-	// }
-}
+	for url, want := range tests {
+		url := url
+		want := want
 
-func ExampleParser_v2() {
-	ctx := context.Background()
-	rcp, err := NewParser().Parse(ctx, "https://delishkitchen.tv/recipes/236903854006862303")
-	if err != nil {
-		panic(err)
+		t.Run(url, func(t *testing.T) {
+			t.Parallel()
+
+			rcp, err := NewParser().Parse(ctx, url)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := common.RecipeMustBe(*rcp, want); err != nil {
+				t.Error(err)
+			}
+		})
 	}
-
-	data, _ := json.MarshalIndent(rcp, "", "  ")
-	fmt.Println(string(data))
-	// output:
-	// {
-	//   "Title": "牛すじ肉の下処理",
-	//   "Image": "https://image.delishkitchen.tv/recipe/236903854006862303/1.jpg?version=1624426802\u0026w=460",
-	//   "IngredientGroups": [
-	//     {
-	//       "Name": "",
-	//       "Children": [
-	//         {
-	//           "Name": "牛すじ肉",
-	//           "Amount": "300g",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "長ねぎ[青い部分]",
-	//           "Amount": "1本分",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "しょうが(薄切り)",
-	//           "Amount": "3枚",
-	//           "Comment": ""
-	//         },
-	//         {
-	//           "Name": "酒",
-	//           "Amount": "50cc",
-	//           "Comment": ""
-	//         }
-	//       ]
-	//     }
-	//   ],
-	//   "Steps": [
-	//     {
-	//       "Text": "鍋に牛すじ肉、牛すじ肉が浸かるくらいの水(分量外:適量)を入れてわかし、弱火で10分程煮る。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/236903854006862303/steps/1.jpg?version=1647222138"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "湯を切り、アクと一緒に余分な脂とアクを流水で洗い流し、食べやすい大きさに切る。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/236903854006862303/steps/2.jpg?version=1647222138"
-	//       ]
-	//     },
-	//     {
-	//       "Text": "鍋も一度洗い、牛すじ肉を戻し入れ、牛すじ肉が浸かるくらいの水(分量外:適量)、ねぎ、しょうが、酒を入れてわかし、アクを取り除きながら1時間程弱火で煮る。",
-	//       "Images": [
-	//         "https://media.delishkitchen.tv/recipe/236903854006862303/steps/3.jpg?version=1647222138"
-	//       ]
-	//     }
-	//   ]
-	// }
 }
