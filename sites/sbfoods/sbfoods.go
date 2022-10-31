2104c4bdb247f5d83c9fa956f927a865af536892
psyark <psyark@gmail.com>
2022-10-31 10:34:25 +0900

    sbfoodsのParser2対応

--- sites/sbfoods/sbfoods.go
+++ sites/sbfoods/sbfoods.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/psyark/recipebot/recipe"
+	"github.com/psyark/recipebot/rexch"
 	"github.com/psyark/recipebot/sites"
 
 	"github.com/PuerkitoBio/goquery"
@@ -16,6 +17,14 @@ var stepRegex = regexp.MustCompile(`^【[１２３４５６７８９０]+】`)
 type parser struct{}
 
 func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) {
+	rex, err := p.Parse2(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	return rex.BackCompat(), nil
+}
+
+func (p *parser) Parse2(ctx context.Context, url string) (*rexch.Recipe, error) {
 	if !strings.HasPrefix(url, "https://www.sbfoods.co.jp/recipe/detail/") {
 		return nil, sites.ErrUnsupportedURL
 	}
@@ -25,7 +34,7 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 		return nil, err
 	}
 
-	rcp := &recipe.Recipe{
+	rex := &rexch.Recipe{
 		Title: strings.TrimSpace(doc.Find(`h1`).Text()),
 		Image: doc.Find(`.detail-img img`).AttrOr("src", ""),
 	}
@@ -35,24 +44,28 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 		if s.Find(".data").Length() == 0 {
 			groupName = strings.TrimSpace(s.Find(".title").Text())
 		} else {
-			rcp.AddIngredient(groupName, recipe.Ingredient{
+			igd := rexch.Ingredient{
+				Group:  groupName,
 				Name:   debrand(strings.TrimSpace(s.Find(".title").Text())),
 				Amount: strings.TrimSpace(s.Find(".data").Text()),
-			})
+			}
+			rex.Ingredients = append(rex.Ingredients, igd)
 		}
 	})
 
 	doc.Find(`#box-howto li`).Each(func(i int, s *goquery.Selection) {
-		rcp.Steps = append(rcp.Steps, recipe.Step{Text: stepRegex.ReplaceAllString(strings.TrimSpace(s.Text()), "")})
+		ist := rexch.Instruction{}
+		ist.AddText(stepRegex.ReplaceAllString(strings.TrimSpace(s.Text()), ""))
+		rex.Instructions = append(rex.Instructions, ist)
 	})
 
-	return rcp, nil
+	return rex, nil
 }
 
 func debrand(name string) string {
 	return strings.TrimPrefix(name, "S&B")
 }
 
-func NewParser() sites.Parser {
+func NewParser() sites.Parser2 {
 	return &parser{}
 }
