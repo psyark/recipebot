c9fe8a41b1d0af2d6bcc81991c80ab5eef35d8d0
psyark <psyark@gmail.com>
2022-10-31 10:19:36 +0900

    dancyuのRecipe2対応

--- sites/dancyu/dancyu.go
+++ sites/dancyu/dancyu.go
@@ -8,6 +8,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/psyark/jsonld"
 	"github.com/psyark/recipebot/recipe"
+	"github.com/psyark/recipebot/rexch"
 	"github.com/psyark/recipebot/sites"
 )
 
@@ -20,6 +21,14 @@ var (
 type parser struct{}
 
 func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) {
+	rex, err := p.Parse2(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	return rex.BackCompat(), nil
+}
+
+func (p *parser) Parse2(ctx context.Context, url string) (*rexch.Recipe, error) {
 	if !strings.HasPrefix(url, "https://dancyu.jp/recipe/") {
 		return nil, sites.ErrUnsupportedURL
 	}
@@ -29,7 +38,7 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 		return nil, err
 	}
 
-	rcp := &recipe.Recipe{}
+	rex := &rexch.Recipe{}
 	var parseError error
 
 	doc.Find(`script[type="application/ld+json"]`).EachWithBreak(func(i int, s *goquery.Selection) bool {
@@ -46,18 +55,18 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 		if rcp2, ok := obj2.(*jsonld.Recipe); ok {
 			for _, name := range rcp2.Name {
 				if name, ok := name.(string); ok {
-					rcp.Title = name
+					rex.Title = name
 				}
 			}
 
 			for _, image := range rcp2.Image {
 				switch image := image.(type) {
 				case string:
-					rcp.Image = sites.ResolvePath(url, strings.TrimSpace(image))
+					rex.Image = sites.ResolvePath(url, strings.TrimSpace(image))
 				case *jsonld.ImageObject:
 					for _, imgURL := range image.Url {
 						if imgURL, ok := imgURL.(string); ok {
-							rcp.Image = sites.ResolvePath(url, strings.TrimSpace(imgURL))
+							rex.Image = sites.ResolvePath(url, strings.TrimSpace(imgURL))
 						}
 					}
 				}
@@ -80,11 +89,11 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 			}
 
 			if hasPrefixA == 1 && hasPrefixDot == 0 {
-				p.parseIngrediens3(ingredients, rcp)
+				p.parseIngrediens3(ingredients, rex)
 			} else if hasPrefixA > 1 {
-				p.parseIngrediens2(ingredients, rcp)
+				p.parseIngrediens2(ingredients, rex)
 			} else {
-				p.parseIngrediens1(ingredients, rcp)
+				p.parseIngrediens1(ingredients, rex)
 			}
 			return false // 1ページに複数レシピがある場合があるので必ず1個目で中止
 		}
@@ -97,34 +106,35 @@ func (p *parser) Parse(ctx context.Context, url string) (*recipe.Recipe, error) 
 	}
 
 	// タイトルの引用符内側
-	if match := titleRegex.FindStringSubmatch(rcp.Title); len(match) == 2 {
-		rcp.Title = match[1]
+	if match := titleRegex.FindStringSubmatch(rex.Title); len(match) == 2 {
+		rex.Title = match[1]
 	}
 
 	// 手順の画像がJSON-LDに含まれないのでHTMLをパース
 	const stepSelector = `.snippet__freearea__subtitle-number,.block__snippet__freeText`
 	doc.Find(`div.block__snippet`).ChildrenFiltered(stepSelector).Each(func(i int, s *goquery.Selection) {
-		step := recipe.Step{Text: stepRegex.ReplaceAllString(strings.TrimSpace(s.Text()), "")}
+		ist := rexch.Instruction{}
+		ist.AddText(stepRegex.ReplaceAllString(strings.TrimSpace(s.Text()), ""))
 		s.NextUntil(stepSelector).Each(func(i int, s *goquery.Selection) {
 			cls := s.AttrOr("class", "")
 			if cls == "snippet__freearea__simple-text" {
-				step.Text += "\n" + s.Text()
+				ist.AddText(s.Text())
 			} else if strings.Contains(cls, "block__snippet__column") {
 				s.Find("img").Each(func(i int, s *goquery.Selection) {
-					step.Images = append(step.Images, sites.ResolvePath(url, s.AttrOr("data-src", "")))
+					ist.AddImage(sites.ResolvePath(url, s.AttrOr("data-src", "")))
 				})
 			}
 		})
-		rcp.Steps = append(rcp.Steps, step)
+		rex.Instructions = append(rex.Instructions, ist)
 	})
 
-	return rcp, nil
+	return rex, nil
 }
 
 // パターン1
 // https://dancyu.jp/recipe/2021_00005129.html
 // 中黒は第2レベル、"A" は列挙の開始時に置かれる
-func (p *parser) parseIngrediens1(ingredients []string, rcp *recipe.Recipe) {
+func (p *parser) parseIngrediens1(ingredients []string, rex *rexch.Recipe) {
 	group := ""
 	for i, item := range ingredients {
 		pair := strings.SplitN(item, "：", 2)
@@ -138,24 +148,25 @@ func (p *parser) parseIngrediens1(ingredients []string, rcp *recipe.Recipe) {
 			}
 		}
 
-		idg := recipe.Ingredient{Name: th, Amount: td}
-		if strings.HasPrefix(idg.Name, "・") {
-			idg.Name = strings.TrimSpace(strings.TrimPrefix(idg.Name, "・"))
+		igd := rexch.Ingredient{Name: th, Amount: td}
+		if strings.HasPrefix(igd.Name, "・") {
+			igd.Name = strings.TrimSpace(strings.TrimPrefix(igd.Name, "・"))
 		} else {
 			group = ""
 		}
-		if match := commentRegex.FindStringSubmatch(idg.Amount); len(match) == 3 {
-			idg.Amount = match[1]
-			idg.Comment = match[2]
+		if match := commentRegex.FindStringSubmatch(igd.Amount); len(match) == 3 {
+			igd.Amount = match[1]
+			igd.Comment = match[2]
 		}
-		rcp.AddIngredient(group, idg)
+		igd.Group = group
+		rex.Ingredients = append(rex.Ingredients, igd)
 	}
 }
 
 // パターン2
 // https://dancyu.jp/recipe/2019_00001391.html
 // 中黒はトップレベル、"A" は各アイテムに付く
-func (p *parser) parseIngrediens2(ingredients []string, rcp *recipe.Recipe) {
+func (p *parser) parseIngrediens2(ingredients []string, rex *rexch.Recipe) {
 	for _, item := range ingredients {
 		group := ""
 		pair := strings.SplitN(item, "：", 2)
@@ -170,21 +181,23 @@ func (p *parser) parseIngrediens2(ingredients []string, rcp *recipe.Recipe) {
 			th = pair[1]
 		}
 
-		idg := recipe.Ingredient{Name: th, Amount: td}
+		igd := rexch.Ingredient{Name: th, Amount: td}
 
-		if match := commentRegex.FindStringSubmatch(idg.Amount); len(match) == 3 {
-			idg.Amount = match[1]
-			idg.Comment = match[2]
+		if match := commentRegex.FindStringSubmatch(igd.Amount); len(match) == 3 {
+			igd.Amount = match[1]
+			igd.Comment = match[2]
 		}
 
-		rcp.AddIngredient(group, idg)
+		igd.Group = group
+
+		rex.Ingredients = append(rex.Ingredients, igd)
 	}
 }
 
 // パターン3
 // https://dancyu.jp/recipe/2020_00003873.html
 // "A" はグループ名に付くが中黒は使わない
-func (p *parser) parseIngrediens3(ingredients []string, rcp *recipe.Recipe) {
+func (p *parser) parseIngrediens3(ingredients []string, rex *rexch.Recipe) {
 	group := ""
 	for _, item := range ingredients {
 		pair := strings.SplitN(item, "：", 2)
@@ -194,18 +207,19 @@ func (p *parser) parseIngrediens3(ingredients []string, rcp *recipe.Recipe) {
 		if strings.HasPrefix(th, "A ") || strings.HasPrefix(th, "B ") || strings.HasPrefix(th, "C ") {
 			group = th
 		} else {
-			idg := recipe.Ingredient{Name: th, Amount: td}
+			igd := rexch.Ingredient{Name: th, Amount: td}
 
-			if match := commentRegex.FindStringSubmatch(idg.Amount); len(match) == 3 {
-				idg.Amount = match[1]
-				idg.Comment = match[2]
+			if match := commentRegex.FindStringSubmatch(igd.Amount); len(match) == 3 {
+				igd.Amount = match[1]
+				igd.Comment = match[2]
 			}
 
-			rcp.AddIngredient(group, idg)
+			igd.Group = group
+			rex.Ingredients = append(rex.Ingredients, igd)
 		}
 	}
 }
 
-func NewParser() sites.Parser {
+func NewParser() sites.Parser2 {
 	return &parser{}
 }
